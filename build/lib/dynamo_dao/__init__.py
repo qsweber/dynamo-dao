11b2cb1a80e14484cbb3f100eca0f49643e7e98e
Quinn Weber <quinn@quinnweber.com>
2020-12-22 08:48:48 -0800

    remove build

--- build/lib/dynamo_dao/__init__.py
+++
@@ -1,5 +0,0 @@
-from dynamo_dao.dao import Dao as _DaoImported
-from dynamo_dao.clients.dynamo import DynamoObject as _DynamoObjectImported
-
-Dao = _DaoImported
-DynamoObject = _DynamoObjectImported
